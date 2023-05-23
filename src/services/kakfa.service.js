0b5e391cc39bf03874a4cc628d22e33044b1e002
IvanAVila1224 <magista1224@gmail.com>
2023-05-22 21:49:49 -0600

    up v5 finalapp

--- src/services/kakfa.service.js
+++ src/services/kakfa.service.js
@@ -1,23 +1,23 @@
 class KafkaService {
-   //url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
-   url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net/'
   
+   url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net/';
+ 
    reaction = async (name) => {
-    await fetch(this.url + '/like?name=' + name, {
+     await fetch(this.url + '/like?name=' + name, {
        method: 'GET',
        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-       },  
-    })  
-       .then((response) => console.log(response.json()))
+         'Content-type': 'application/json; charset=UTF-8',
+       },
+     })
+       .then((response) => response.json())
        .then((data) => {
          console.log(data);
-       })  
+       })
        .catch((err) => {
-          console.log(err.message);
-       }); 
-   }
- 
+         console.log(err.message);
+       });
+   };
  }
  
- export default new KafkaService();
+ const kafkaService = new KafkaService();
+ export default kafkaService;
