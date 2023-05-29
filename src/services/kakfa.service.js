d4361b939b36e1af3d7427be0c1c4871566839d6
IvanAVila1224 <magista1224@gmail.com>
2023-05-29 11:50:39 -0600

    reacctions and comments v4

--- src/services/kakfa.service.js
+++ src/services/kakfa.service.js
@@ -1,9 +1,9 @@
 class KafkaService {
   
-   url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net';
+   url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net/';
  
    reaction = async (userid, objectid, reactionid) => {
-    await fetch(this.url + '/reactions?userid=' + userid+'&objectid='+objectid +'&reactionid='+reactionid, {
+    await fetch(this.url + 'reactions?userid=' + userid+'&objectid='+objectid +'&reactionid='+reactionid, {
       method: 'GET',
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
@@ -19,7 +19,7 @@ class KafkaService {
   };
 
   comment = async (userid, objectid, message) => {
-    await fetch(this.url + '/comments?userid=' + userid+'&objectid='+objectid +'&message='+message, {
+    await fetch(this.url + 'comments?userid=' + userid+'&objectid='+objectid +'&message='+message, {
       method: 'GET',
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
