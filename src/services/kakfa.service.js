bfa0d4ea795659e184c7d6c7dc0528624e4d4917
IvanAVila1224 <magista1224@gmail.com>
2023-05-30 18:57:08 -0600

    update reacction 4

--- src/services/kakfa.service.js
+++ src/services/kakfa.service.js
@@ -1,22 +1,29 @@
 class KafkaService {
-  
-   url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net/';
- 
-   reaction = async (userid, objectid, reactionid) => {
-    await fetch(this.url + '/reactions?userid=' + userid+'&objectid='+objectid +'&reactionid='+reactionid, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
+  url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net';
+
+  reaction = async (userid, objectid, reactionid) => {
+    const url = `${this.url}/reactions?userid=${userid}&objectid=${objectid}&reactionid=${reactionid}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
       });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+      } else {
+        throw new Error('Request failed with status ' + response.status);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
+
+
 
   comment = async (userid, objectid, message) => {
     await fetch(this.url + '/comments?userid=' + userid+'&objectid='+objectid +'&message='+message, {
