de7e2c88f9a81e9a50ac2929d16013d7ba25c33b
IvanAVila1224 <magista1224@gmail.com>
2023-05-30 18:33:46 -0600

    update reacction

--- src/services/kakfa.service.js
+++ src/services/kakfa.service.js
@@ -2,22 +2,38 @@ class KafkaService {
   
    url = 'https://nodeproducer-service-v2-ivanavila1224.cloud.okteto.net/';
  
-   reaction = async (name) => {
-    await fetch(this.url + 'like?name=' + name, {
-       method: 'GET',
-       headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-       },  
-    })  
-       .then((response) => console.log(response.json()))
-       .then((data) => {
-         console.log(data);
-       })  
-       .catch((err) => {
-          console.log(err.message);
-       }); 
-   }
- 
- }
- // eslint-disable-next-line
- export default new KafkaService();
+   reaction = async (userid, objectid, reactionid) => {
+    await fetch(this.url + '/reactions?userid=' + userid+'&objectid='+objectid +'&reactionid='+reactionid, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  comment = async (userid, objectid, message) => {
+    await fetch(this.url + '/comments?userid=' + userid+'&objectid='+objectid +'&message='+message, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+}
+
+const kafkaService = new KafkaService();
+export default kafkaService;
